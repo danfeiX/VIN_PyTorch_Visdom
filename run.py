16cd55aa1120cfcec0ffed3c0f8eac145e944808
Danfei Xu <morphine1021@gmail.com>
2020-06-20 20:40:47 -0700

    pytorch1.5

--- run.py
+++ run.py
@@ -19,41 +19,41 @@ use_GPU = torch.cuda.is_available()
 # Parsing training parameters
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--datafile', 
-                    type=str, 
-                    default='./data/gridworld_8x8.npz', 
+parser.add_argument('--datafile',
+                    type=str,
+                    default='./data/gridworld_8x8.npz',
                     help='Path to data file')
-parser.add_argument('--imsize', 
-                    type=int, 
-                    default=8, 
+parser.add_argument('--imsize',
+                    type=int,
+                    default=8,
                     help='Size of image')
-parser.add_argument('--lr', 
-                    type=float, 
-                    default=0.002, 
+parser.add_argument('--lr',
+                    type=float,
+                    default=0.002,
                     help='Learning rate, [0.01, 0.005, 0.002, 0.001]')
-parser.add_argument('--epochs', 
-                    type=int, 
-                    default=30, 
+parser.add_argument('--epochs',
+                    type=int,
+                    default=30,
                     help='Number of epochs to train')
-parser.add_argument('--k', 
-                    type=int, 
-                    default=10, 
+parser.add_argument('--k',
+                    type=int,
+                    default=10,
                     help='Number of Value Iterations')
-parser.add_argument('--ch_i', 
-                    type=int, 
-                    default=2, 
+parser.add_argument('--ch_i',
+                    type=int,
+                    default=2,
                     help='Number of channels in input layer')
-parser.add_argument('--ch_h', 
-                    type=int, 
-                    default=150, 
+parser.add_argument('--ch_h',
+                    type=int,
+                    default=150,
                     help='Number of channels in first hidden layer')
-parser.add_argument('--ch_q', 
-                    type=int, 
-                    default=10, 
+parser.add_argument('--ch_q',
+                    type=int,
+                    default=10,
                     help='Number of channels in q layer (~actions) in VI-module')
-parser.add_argument('--batch_size', 
-                    type=int, 
-                    default=128, 
+parser.add_argument('--batch_size',
+                    type=int,
+                    default=128,
                     help='Batch size') # TODO: Divisibility to DataLoader
 
 args = parser.parse_args()
@@ -63,7 +63,7 @@ net = VIN(args)
 
 if use_GPU:
      net = net.cuda()
-        
+
 # Loss
 criterion = nn.CrossEntropyLoss()
 
@@ -71,7 +71,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.RMSprop(net.parameters(), lr=args.lr, eps=1e-6)
 
 # Dataset transformer: torchvision.transforms
-transform = None # 
+transform = None #
 
 # Define Dataset
 trainset = GridworldData(args.datafile, imsize=args.imsize, train=True, transform=transform)
@@ -83,7 +83,7 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, sh
 
 for epoch in range(args.epochs): # Loop over dataset multiple times
     running_losses = []
-    
+
     start_time = time.time()
     for i, data in enumerate(trainloader): # Loop over batches of data
         # Get input batch
@@ -91,39 +91,40 @@ for epoch in range(args.epochs): # Loop over dataset multiple times
 
         if X.size()[0] != args.batch_size: # TODO: Bug with DataLoader
             continue # Drop those data, if not enough for a batch
-        
+
         # Send Tensors to GPU if available
         if use_GPU:
             X = X.cuda()
             S1 = S1.cuda()
             S2 = S2.cuda()
             labels = labels.cuda()
-            
+
         # Wrap to autograd.Variable
         X, S1, S2, labels = Variable(X), Variable(S1), Variable(S2), Variable(labels)
-        
+
         # Zero the parameter gradients
         optimizer.zero_grad()
-        
+
         # Forward pass
         outputs = net(X, S1, S2, args)
-        
+
         # Loss
         loss = criterion(outputs, labels)
-        
+
         # Backward pass
         loss.backward()
-        
+
         # Update params
         optimizer.step()
-        
+
         # Accumulate running losses
-        running_losses.append(loss.data[0]) # Take out value from 1D Tensor
-        
+
+        running_losses.append(loss.item()) # Take out value from 1D Tensor
+
     time_duration = time.time() - start_time
     # Print epoch logs
     print('[Epoch # {:3d} ({:.1f} s)] Loss: {:.4f}'.format(epoch + 1, time_duration, np.mean(running_losses)))
-    
+
 print('\nFinished training. \n')
 
 # Testing...
@@ -133,7 +134,7 @@ total = 0
 for i, data in enumerate(testloader):
     # Get inputs
     X, S1, S2, labels = data
-    
+
     if X.size()[0] != args.batch_size: # TODO: Bug with DataLoader
         continue # Drop those data, if not enough for a batch
 
@@ -143,23 +144,23 @@ for i, data in enumerate(testloader):
         S1 = S1.cuda()
         S2 = S2.cuda()
         labels = labels.cuda()
-        
+
     # Wrap to autograd.Variable
     X, S1, S2 = Variable(X), Variable(S1), Variable(S2)
 
     # Forward pass
     outputs = net(X, S1, S2, args)
-    
+
     # Select actions with max scores(logits)
     _, predicted = torch.max(outputs, dim=1)
-    
+
     # Unwrap autograd.Variable to Tensor
     predicted = predicted.data
 
     # Compute test accuracy
     correct += (predicted == labels).sum()
     total += labels.size()[0] # args.batch_size*num_batches, TODO: Check if DataLoader drop rest examples less than batch_size
-    
+
 print('Test Accuracy (with {:d} examples): {:.2f}%'.format(total, 100*(correct/total)))
 print('\nFinished testing.\n')
 
